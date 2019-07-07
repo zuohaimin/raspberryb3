5de6a2df540e745a3610fd2faf7c8297d765a28e
zuohaimin <2975069636@qq.com>
2019-07-07 22:36:47 +0800

    添加发送短信的功能

--- src/main/java/cn/edu/swpu/raspberryb3/constant/RaspberryConstant.java
+++ src/main/java/cn/edu/swpu/raspberryb3/constant/RaspberryConstant.java
@@ -9,5 +9,17 @@ public class RaspberryConstant {
 
     public static final int BUZZER_RATE  = 1;
 
-    public static final String MOBILE_NUM = "15228318140";
+    public static final String TEMPLATEID = "1759108513";
+
+    /**
+     *     "https://openapi.miaodiyun.com/distributor/sendSMS"
+     *     bug : 猜测是没有充值token
+     */
+    public final static String SMS_URL = "https://api.miaodiyun.com/20150822/industrySMS/sendSMS";
+
+    public final static String ACCOUNT_SID = "c4d1fc3fb61c444880383858b6c719ae";
+
+    public final static String AUTH_TOKEN = "8dd7c07eaa154e498fc842d0dec4a507";
+
+    public static final String RESP_DATA_TYPE = "JSON";
 }
--- src/main/java/cn/edu/swpu/raspberryb3/utils/SmsSender.java
+++ src/main/java/cn/edu/swpu/raspberryb3/utils/SmsSender.java
@@ -1,9 +1,18 @@
 package cn.edu.swpu.raspberryb3.utils;
 
+
 import cn.edu.swpu.raspberryb3.constant.RaspberryConstant;
 
-import java.io.UnsupportedEncodingException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.URLEncoder;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author: 束手就擒
@@ -13,37 +22,103 @@ import java.net.URLEncoder;
  */
 public class SmsSender {
 
-    private final static String URL = "http://api01.monyun.cn:7901/sms/v2/std/single_send";
 
-    private final static String APIKEY = "79d877f035bb25aadd5b2fa6174d9ceb";
 
     /**
      *
-     * @param content
      * @return
-     * 参数拼接：{"apikey":"abade5589e2798f82f006bbc36d269ce","mobile":"138xxxxxxxx","content":"%d1%e9%d6%a"}
+     * 参数拼接：accountSid=7727bb0e0f9b910c48fc1ec5e3xxxxxx&to=186xxxxxxxx
      */
-    private String getQueryArgs(String content){
-        String contentEncode = null;
-        try {
-             contentEncode = URLEncoder.encode(content,"GBK");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+    private static String getQueryArgs(String accountSid, List<String> to, long timestamp, String sig,
+                                       String templateid, String respDataType){
         StringBuilder sb = new StringBuilder();
-        sb.append("{\"apikey\":\"");
-        sb.append(APIKEY);
-        sb.append("\",\"mobile\":\"");
-        sb.append(RaspberryConstant.MOBILE_NUM);
-        sb.append("\",\"content\":\"");
-        sb.append(contentEncode);
-        sb.append("\"}");
+        //accountSid,to,timestamp,sig
+        sb.append("accountSid=").append(accountSid);
+        sb.append("&to=");
+        for (String s : to){
+            sb.append(s).append(",");
+        }
+        //删除最后的一个英文逗号
+        sb.deleteCharAt(sb.lastIndexOf(","));
+        sb.append("&timestamp=").append(timestamp);
+        sb.append("&sig=").append(sig);
+        sb.append("&templateid=").append(templateid);
+        sb.append("&respDataType=").append(respDataType);
         return sb.toString();
     }
 
+    private static long getTimestamp(){
+        return System.currentTimeMillis();
+    }
 
-    public static void main(String[] args) {
-        SmsSender smsSender = new SmsSender();
-        System.out.println(smsSender.getQueryArgs("你好"));
+    /**
+     * @param sid
+     * @param token
+     * @param timestamp
+     * @return
+     * md5 加密
+     */
+    private static String getMd5(String sid, String token, long timestamp){
+       StringBuilder sb = new StringBuilder();
+       String source = sid+token+timestamp;
+
+        try {
+                MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+                byte[] bytes = messageDigest.digest(source.getBytes());
+                for (byte b : bytes){
+                    String hex = Integer.toHexString(b&0xff);
+                    if (hex.length() == 1){
+                        sb.append("0").append(hex);
+                    }else {
+                        sb.append(hex);
+                    }
+                }
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 谨慎调用，余额只有一个机会了，秒滴云
+     * @param to
+     * @return
+     */
+    public static String sendSms(List<String> to){
+        long timestamp = getTimestamp();
+        String sig = getMd5(RaspberryConstant.ACCOUNT_SID,RaspberryConstant.AUTH_TOKEN,timestamp);
+        OutputStreamWriter outputStreamWriter;
+        BufferedReader br;
+        StringBuilder result = new StringBuilder();
+        try {
+
+            URL url = new URL(RaspberryConstant.SMS_URL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(10000);
+            connection.setRequestProperty("Content-type", "application/x-www-form-urlencoded");
+            outputStreamWriter = new OutputStreamWriter(connection.getOutputStream(),"utf-8");
+            String args = getQueryArgs(RaspberryConstant.ACCOUNT_SID,to,timestamp,sig,RaspberryConstant.TEMPLATEID,RaspberryConstant.RESP_DATA_TYPE);
+            System.out.println(args);
+            outputStreamWriter.write(args);
+            outputStreamWriter.flush();
+
+            //读取返回的结果
+            br = new BufferedReader(new InputStreamReader(connection.getInputStream(), "utf-8"));
+            String temp = "";
+            while ((temp = br.readLine()) !=null){
+                result.append(temp);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result.toString();
+
     }
 }

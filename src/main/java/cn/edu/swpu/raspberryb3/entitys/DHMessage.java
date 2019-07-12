bdb7060ebfb72a3f795f92a6711a443f6e65441b
zuohaimin <2975069636@qq.com>
2019-07-12 13:25:02 +0800

    dht11

--- src/main/java/cn/edu/swpu/raspberryb3/entitys/DHMessage.java
+++ src/main/java/cn/edu/swpu/raspberryb3/entitys/DHMessage.java
@@ -1,11 +1,11 @@
 package cn.edu.swpu.raspberryb3.entitys;
 
-import com.sun.xml.internal.ws.spi.db.DatabindingException;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @Author: 束手就擒
@@ -20,37 +20,6 @@ public class DHMessage implements Serializable{
     private int id;
     private Double tempetature;
     private Double humidity;
-    private DatabindingException time;
+    private Date time;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Double getTempetature() {
-        return tempetature;
-    }
-
-    public void setTempetature(Double tempetature) {
-        this.tempetature = tempetature;
-    }
-
-    public Double getHumidity() {
-        return humidity;
-    }
-
-    public void setHumidity(Double humidity) {
-        this.humidity = humidity;
-    }
-
-    public DatabindingException getTime() {
-        return time;
-    }
-
-    public void setTime(DatabindingException time) {
-        this.time = time;
-    }
 }
